d09511688acab4c1cc21f7d8deac695ed1814563
Chris Fordham <chris@xhost.com.au>
2014-03-26 16:30:29 +1100

    Use single quotes in metadata.rb header.

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
 name             'boto'
 maintainer       'Chris Fordham'
 maintainer_email 'cookbooks@xhost.com.au'
-license          "Apache 2.0"
-description      "Installs/Configures boto"
+license          'Apache 2.0'
+description      'Installs/Configures boto.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          '0.0.1'
 
-depends          "python"
+depends          'python'
 
 recipe "boto::configure", "Configures boto."
 recipe "boto::default", "Installs & configures boto."
